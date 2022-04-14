ef4bd3702b47e7a636d646c68d01e346b24d805f
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-04-14 16:51:12 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,26 @@ import { ContactList } from './Contact list/ContactList';
 import { Filter } from './Filter/Filter';
 import { Global } from './Global';
 
+const KEY = "contacts"
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount(){
+    this.setState({
+      contacts: JSON.parse(localStorage.getItem(KEY))
+    })
+    
+  }
+
+  componentDidUpdate(_, prevState){
+    if(prevState.contacts !== this.state.contacts){
+      localStorage.setItem(KEY, JSON.stringify(this.state.contacts))
+    }
+  }
 
   addNewContact = ({ name, number }) => {
     if (
